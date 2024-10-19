bfedc82027b7a72961bf7cc27c210d57190374bd
Scrapman238 <Scrapman238@hotmail.com>
2024-10-19 18:06:12 +0100

    Attempt 11 to fix back loading

--- js/fade-in.js
+++ js/fade-in.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elements = document.querySelectorAll('.fade-in');
     
+    // Function to remove 'animate-fade-in' class from all elements
+    function removeFadeInClasses() {
+        elements.forEach(element => {
+            element.classList.remove('animate-fade-in');
+        });
+    }
+
+    // Function to fade in elements
     function fadeInElements(index) {
         if (index < elements.length) {
             setTimeout(() => {
-                // Remove the class before adding it again to restart the animation
-                elements[index].classList.remove('animate-fade-in');
-                void elements[index].offsetWidth; // Trigger reflow
                 elements[index].classList.add('animate-fade-in');
                 fadeInElements(index + 1); // Call the next element
             }, 50); // Delay for each element
         }
     }
     
+    removeFadeInClasses(); // First, remove any existing classes
     fadeInElements(0); // Start the fading in process
 });
